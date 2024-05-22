37f172559925c0474fe0f4938ee65494d7d070cc
Pedro <pedroazeredo@soulasalle.com.br>
2024-05-22 03:13:48 -0300

    botao de reset

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 'use strict';
 
-const numerosecreto = Math.floor(Math.random() * 20) + 1; //gerar numero aleatorio
+let numerosecreto = Math.floor(Math.random() * 20) + 1; //gerar numero aleatorio
 let score = 20; //vidas
+let highscore = 0;
 
 document.querySelector('.btn_checar').addEventListener('click', function () {
   const adivinhe = Number(document.querySelector('.input').value);
-  console.log(adivinhe, typeof adivinhe);
 
   //quando nao é numero
   if (!adivinhe) {
@@ -14,9 +14,13 @@ document.querySelector('.btn_checar').addEventListener('click', function () {
     //quando acerta
   } else if (adivinhe === numerosecreto) {
     document.querySelector('.feedback').textContent = 'Acertou! ✅';
-
     document.querySelector('body').style.backgroundColor = '#60b347'; //muda cor do bg
     document.querySelector('.textoResultado').textContent = numerosecreto;
+
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = highscore;
+    }
 
     //quando é maior que 20
   } else if (adivinhe > numerosecreto && adivinhe > 20) {
@@ -57,4 +61,13 @@ document.querySelector('.btn_checar').addEventListener('click', function () {
   }
 });
 
-console.log(document.querySelector('.scores span').textContent);
+//faz o botao de novo reiniciar
+document.querySelector('.btn_denovo').addEventListener('click', function () {
+  score = 20;
+  numerosecreto = Math.floor(Math.random() * 20) + 1;
+  document.querySelector('body').style.backgroundColor = '#e2b10c';
+  document.querySelector('.scores span').textContent = score;
+  document.querySelector('.feedback').textContent = 'Tenta a sorte!';
+  document.querySelector('.textoResultado').textContent = '?';
+  document.querySelector('.input').value = '';
+});
