7c03a81cefbce66c706bee46dfc03c8e12a45a10
Pedro <pedroazeredo@soulasalle.com.br>
2024-05-22 05:50:17 -0300

    adicionei animaçoes e audios

--- script.js
+++ script.js
@@ -2,49 +2,66 @@
 
 let numerosecreto = Math.floor(Math.random() * 20) + 1; //gerar numero aleatorio
 let score = 20; //vidas
-let highscore = 0;
+let highscore = 0; //recorde
 let feedback = document.querySelector('.feedback');
-console.log(feedback);
+console.log(numerosecreto);
+function knock() {
+  const img = document.querySelector('.knockout');
+  img.classList.add('active');
+  let sound = new Audio();
+  sound.src = 'midia/knockout.mp3';
+  sound.volume = 1.0;
+  sound.play();
+}
 
+function mgameover() {
+  let sound = new Audio();
+  sound.src = 'midia/gameover.mp3';
+  sound.volume = 1.0;
+  sound.play();
+}
+function merrar() {
+  let sound = new Audio();
+  sound.src = 'midia/death.mp3';
+  sound.volume = 0.1;
+  sound.play();
+}
 function hit() {
   document.querySelector('.img').src = 'midia/cup_hit.png'; //hit
-  // document.querySelector('.img').src = 'midia/cup_good.png'; //good
-  // document.querySelector('.img').src = 'midia/cup_caracter.png'; //default
+  merrar();
+  score--;
+  document.querySelector('.scores span').textContent = score;
 }
 function perder() {
   document.querySelector('.feedback').textContent = 'Suas Vidas acabaram! 💔';
   document.querySelector('.scores span').textContent = 0;
-  document.querySelector('body').style.backgroundColor = 'red';
+  document.querySelector('body').style.backgroundColor = '#c34b29';
+  mgameover();
 }
-function mscore() {
-  score--;
-  document.querySelector('.scores span').textContent = score;
-}
+
 //checa resultado
 document.querySelector('.btn_checar').addEventListener('click', function () {
   const adivinhe = Number(document.querySelector('.input').value);
   //quando nao é numero
   if (!adivinhe) {
     feedback.textContent = 'Não é um número ❌';
-
     //quando acerta
   } else if (adivinhe === numerosecreto) {
-    document.querySelector('.feedback').textContent = 'Acertou! ✅';
+    feedback.textContent = 'Acertou! ✅';
     document.querySelector('body').style.backgroundColor = '#60b347'; //muda cor do bg
     document.querySelector('.textoResultado').textContent = numerosecreto;
     document.querySelector('.img').src = 'midia/cup_good.png';
-
+    knock();
+    //colocar recorde
     if (score > highscore) {
       highscore = score;
       document.querySelector('.highscore').textContent = highscore;
     }
-
-    //quando é maior que 20
+    //quando é maior que 20 e maior que o numero
   } else if (adivinhe > numerosecreto && adivinhe > 20) {
     if (score > 1) {
       feedback.textContent = 'O numero é entre 1 e 20 💬';
       hit();
-      mscore();
     } else {
       perder();
     }
@@ -54,7 +71,6 @@ document.querySelector('.btn_checar').addEventListener('click', function () {
     if (score > 1) {
       feedback.textContent = '🔽 Muito Baixo!';
       hit();
-      mscore();
     } else {
       perder();
     }
@@ -64,12 +80,12 @@ document.querySelector('.btn_checar').addEventListener('click', function () {
     if (score > 1) {
       feedback.textContent = '🔼 Muito Alto!';
       hit();
-      mscore();
     } else {
       perder();
     }
   }
 });
+
 //faz o botao de novo reiniciar
 document.querySelector('.btn_denovo').addEventListener('click', function () {
   score = 20;
@@ -80,4 +96,5 @@ document.querySelector('.btn_denovo').addEventListener('click', function () {
   document.querySelector('.textoResultado').textContent = '?';
   document.querySelector('.input').value = '';
   document.querySelector('.img').src = 'midia/cup_caracter.png';
+  document.querySelector('.knockout').classList.remove('active');
 });
